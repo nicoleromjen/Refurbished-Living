dc800b1af3f169296f201b2f1b26349a89377f61
jako3501 <jako3501@easv365.dk>
2024-09-03 13:59:06 +0200

    home style

--- ourtheme/home.php
+++ ourtheme/home.php
@@ -13,10 +13,12 @@
                 $excerpt = get_the_excerpt();
                 ?>
 
-                <div class="card">
-                <h2 class="card-title"><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
-                <p><?php echo $date; ?> by <?php echo $author; ?></p>
-                <p class="card-text"><?php echo $excerpt; ?></p>
+                <div class="blog-container">
+                    <div class="blog-content">
+                        <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
+                        <p><?php echo $date; ?> by <?php echo $author; ?></p>
+                        <p><?php echo $excerpt; ?></p>
+                    </div>
                 </div>
 
             <?php endwhile; ?>
