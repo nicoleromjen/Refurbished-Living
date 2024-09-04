df4824839db9c222eaa27c6943595287ad6116d6
jako3501 <jako3501@easv365.dk>
2024-09-04 09:46:03 +0200

    Update home.php

--- ourtheme/home.php
+++ ourtheme/home.php
@@ -1,24 +1,26 @@
 <?php get_header(); ?>
 <div class="blog-container">
-    <div class="blog-content row-3">
-        <?php if (have_posts()): ?>
-            <?php while (have_posts()):
-                the_post() ?>
+    <div class="row">
+        <div class="blog-content col-3">
+            <?php if (have_posts()): ?>
+                <?php while (have_posts()):
+                    the_post() ?>
 
-                <?php
-                $url = get_permalink();
-                $title = get_the_title();
-                $date = get_the_date();
-                $author = get_the_author();
-                $excerpt = get_the_excerpt();
-                ?>
+                    <?php
+                    $url = get_permalink();
+                    $title = get_the_title();
+                    $date = get_the_date();
+                    $author = get_the_author();
+                    $excerpt = get_the_excerpt();
+                    ?>
 
-                        <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
-                        <p><?php echo $date; ?> by <?php echo $author; ?></p>
-                        <p><?php echo $excerpt; ?></p>
+                    <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
+                    <p><?php echo $date; ?> by <?php echo $author; ?></p>
+                    <p><?php echo $excerpt; ?></p>
 
-            <?php endwhile; ?>
-        <?php endif; ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
 
