56fefbaf959b8d0d4c9c040ffb05961e0eaae1f0
smw22 <serwes01@easv365.dk>
2024-09-04 11:12:14 +0200

    style

--- ourtheme/front-page.php
+++ ourtheme/front-page.php
@@ -3,7 +3,19 @@
         <?php while(have_posts()): the_post() ?>
 
         <div class="frontpage-container">
-            <?php echo do_shortcode('[contact-form-7 id="cc310c7" title="Support form"]') ?>
+            <!-- <?php echo do_shortcode('[contact-form-7 id="cc310c7" title="Support form"]') ?> -->
+            <div class="categories">
+                <ul class="categories-list">
+                    <li class="categories-item">Outlet</li>
+                    <li class="categories-item">Haven</li>
+                    <li class="categories-item">Dagligstue</li>
+                    <li class="categories-item">Spisestue</li>
+                    <li class="categories-item">Soveværelse</li>
+                    <li class="categories-item">Opbevaring</li>
+                    <li class="categories-item">Kontor</li>
+                    <li class="categories-item">Belysning</li>
+                </ul>
+            </div>
         </div>
         
         <?php endwhile; ?>
