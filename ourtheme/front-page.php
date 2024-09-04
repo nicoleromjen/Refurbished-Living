1b7509e4f43d8469da5d194b1d2be4f50fe8e9c8
smw22 <serwes01@easv365.dk>
2024-09-04 12:15:44 +0200

    subheader

--- ourtheme/front-page.php
+++ ourtheme/front-page.php
@@ -4,19 +4,161 @@
 
         <div class="frontpage-container">
             <!-- <?php echo do_shortcode('[contact-form-7 id="cc310c7" title="Support form"]') ?> -->
-            <div class="categories">
+            <div class="categories-subheader">
                 <ul class="categories-list">
-                    <li class="categories-item">Outlet</li>
-                    <li class="categories-item">Haven</li>
-                    <li class="categories-item">Dagligstue</li>
-                    <li class="categories-item">Spisestue</li>
-                    <li class="categories-item">Soveværelse</li>
-                    <li class="categories-item">Opbevaring</li>
-                    <li class="categories-item">Kontor</li>
-                    <li class="categories-item">Belysning</li>
+                    <!-- <li class="category">Outlet</li>
+                    <li class="category">Haven</li>
+                    <li class="category">Dagligstue</li>
+                    <li class="category">Spisestue</li>
+                    <li class="category">Soveværelse</li>
+                    <li class="category">Opbevaring</li>
+                    <li class="category">Kontor</li> -->
+
+                    <li class="dropdown-category">
+                      <div class="properties-dropdown">
+                        <a class="category" role="button" data-bs-toggle="dropdown">
+                            Outlet
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 2</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 3</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 4</button></a></li>
+                        </ul>
+                      </div>
+                    </li>
+
+                    <li class="dropdown-category">
+                      <div class="properties-dropdown">
+                        <a class="category" role="button" data-bs-toggle="dropdown">
+                            Haven
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 2</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 3</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 4</button></a></li>
+                        </ul>
+                      </div>
+                    </li>
+
+                    <li class="dropdown-category">
+                      <div class="properties-dropdown">
+                        <a class="category" role="button" data-bs-toggle="dropdown">
+                            Dagligstue
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 2</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 3</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 4</button></a></li>
+                        </ul>
+                      </div>
+                    </li>
+
+                    <li class="dropdown-category">
+                      <div class="properties-dropdown">
+                        <a class="category" role="button" data-bs-toggle="dropdown">
+                            Spisestue
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 2</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 3</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 4</button></a></li>
+                        </ul>
+                      </div>
+                    </li>
+
+                    <li class="dropdown-category">
+                      <div class="properties-dropdown">
+                        <a class="category" role="button" data-bs-toggle="dropdown">
+                            Soveværelse
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 2</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 3</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 4</button></a></li>
+                        </ul>
+                      </div>
+                    </li>
+
+                    <li class="dropdown-category">
+                      <div class="properties-dropdown">
+                        <a class="category" role="button" data-bs-toggle="dropdown">
+                            Opbevaring
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 2</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 3</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 4</button></a></li>
+                        </ul>
+                      </div>
+                    </li>
+
+                    <li class="dropdown-category">
+                      <div class="properties-dropdown">
+                        <a class="category" role="button" data-bs-toggle="dropdown">
+                            Kontor
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 2</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 3</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 4</button></a></li>
+                        </ul>
+                      </div>
+                    </li>
+
+                    <li class="dropdown-category">
+                      <div class="properties-dropdown">
+                        <a class="category" role="button" data-bs-toggle="dropdown">
+                            Belysning
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 2</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 3</button></a></li>
+                          <li><a class="category-item" href=""><button class="dropdown-item" type="button">Item 4</button></a></li>
+                        </ul>
+                      </div>
+                    </li>
+
                 </ul>
             </div>
         </div>
+
+        <!-- <nav>
+                <ul class="nav-list">
+                    <li class="nav-element"><a class="nav-links" href="index.html">HOME</a></li>
+
+                    <li class="dropdown-nav">
+                      <div class="properties-dropdown">
+                        <a class="nav-links"role="button" data-bs-toggle="dropdown">
+                          ACCOMODATION
+                        </a>
+                        <ul class="dropdown-menu">
+                          <li><a class="dropdown-menu-item" href="caja-blanca.html"><button class="dropdown-item" type="button">Caja Blanca</button></a></li>
+                          <li><a class="dropdown-menu-item" href="rosa-villa.html"><button class="dropdown-item" type="button">Rosa Villa</button></a></li>
+                          <li><a class="dropdown-menu-item" href="marina-del-este.html"><button class="dropdown-item" type="button">Marina Del Este</button></a></li>
+                          <li><a class="dropdown-menu-item" href="la-bolichera.html"><button class="dropdown-item" type="button">La Bolichera</button></a></li>
+                        </ul>
+                      </div>
+                    </li>
+
+                    <li class="nav-element"><a class="nav-links" href="deals.html">DEALS</a></li>
+                    <li class="nav-element"><a class="nav-links" href="management.html">MANAGEMENT</a></li>
+                    <li class="nav-element"><a class="nav-links" href="about-us.html">ABOUT US</a></li>
+                    <li class="nav-element"><a class="nav-links" href="contact.html">CONTACT</a></li>
+
+                    <li class="nav-element nav-element-last-child"><a class="nav-links nav-links-last-child" href="index.html"> <img src="../img/bandera-reino-unido.svg" alt="" class="banderas"> </a>
+                     | <a class="nav-links nav-links-last-child" href="index-es.html"> <img src="../img/bandera-españa.avif" alt="" class="banderas"> </a> 
+                     |  <a class="nav-links nav-links-last-child" href="index-da.html"> <img src="../img/bandera-dinamarca.avif" alt="" class="banderas"> </a>
+                    </li>
+                </ul>
+            </nav> -->
         
         <?php endwhile; ?>
     <?php endif; ?>
--- ourtheme/functions.php
+++ ourtheme/functions.php
@@ -2,6 +2,7 @@
 function refurbished_register_stylesheet() {
     wp_enqueue_style("bootstrap", "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css");
     wp_enqueue_style("style", get_stylesheet_directory_uri() . "/style.css");
+    wp_enqueue_script("bootstrap-js", "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js");
 }
 add_action("wp_enqueue_scripts", "refurbished_register_stylesheet");
 
