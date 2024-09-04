f1ab36d5866921ac38653b52d8eda7a00c2f7142
jako3501 <jako3501@easv365.dk>
2024-09-04 10:12:04 +0200

    Update home.php

--- ourtheme/home.php
+++ ourtheme/home.php
@@ -1,47 +1,32 @@
 <?php get_header(); ?>
+
+
+<div class="blog-container">
     <?php if (have_posts()): ?>
-        <?php while (have_posts()):the_post() ?>
-
-            <div class="blog-container">
-                <div class="row">
-                    <div class="blog-content col-3">
-
-
-                        <?php
-                        $url = get_permalink();
-                        $title = get_the_title();
-                        $date = get_the_date();
-                        $author = get_the_author();
-                        $excerpt = get_the_excerpt();
-                        ?>
-
-                        <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
-                        <p><?php echo $date; ?> by <?php echo $author; ?></p>
-                        <p><?php echo $excerpt; ?></p>
+        <?php while (have_posts()):
+            the_post() ?>
+            <div class="row">
+                <div class="blog-content col-3">
 
 
-                    </div>
+                    <?php
+                    $url = get_permalink();
+                    $title = get_the_title();
+                    $date = get_the_date();
+                    $author = get_the_author();
+                    $excerpt = get_the_excerpt();
+                    ?>
 
-                    <div class="blog-content row-3">
-
-
-                        <?php
-                        $url = get_permalink();
-                        $title = get_the_title();
-                        $date = get_the_date();
-                        $author = get_the_author();
-                        $excerpt = get_the_excerpt();
-                        ?>
-
-                        <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
-                        <p><?php echo $date; ?> by <?php echo $author; ?></p>
-                        <p><?php echo $excerpt; ?></p>
+                    <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
+                    <p><?php echo $date; ?> by <?php echo $author; ?></p>
+                    <p><?php echo $excerpt; ?></p>
 
 
-                    </div>
                 </div>
             </div>
-
         <?php endwhile; ?>
     <?php endif; ?>
+</div>
+
+
 <?php get_footer(); ?>
