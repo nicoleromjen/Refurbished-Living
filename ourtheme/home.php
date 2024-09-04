ba4754086a3e55f4835cb44ea569384064a34c4a
smw22 <serwes01@easv365.dk>
2024-09-04 10:05:12 +0200

    Update home.php

--- ourtheme/home.php
+++ ourtheme/home.php
@@ -1,31 +1,46 @@
 <?php get_header(); ?>
+    <?php if (have_posts()): ?>
+        <?php while (have_posts()):the_post() ?>
 
-<?php if (have_posts()): ?>
-    <?php while (have_posts()):
-        the_post() ?>
-        <div class="blog-container">
-            <div class="row">
-                <div class="blog-content col-3">
-
-
-                    <?php
-                    $url = get_permalink();
-                    $title = get_the_title();
-                    $date = get_the_date();
-                    $author = get_the_author();
-                    $excerpt = get_the_excerpt();
-                    ?>
-
-                    <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
-                    <p><?php echo $date; ?> by <?php echo $author; ?></p>
-                    <p><?php echo $excerpt; ?></p>
+            <div class="blog-container">
+                <div class="row">
+                    <div class="blog-content col-3">
 
 
+                        <?php
+                        $url = get_permalink();
+                        $title = get_the_title();
+                        $date = get_the_date();
+                        $author = get_the_author();
+                        $excerpt = get_the_excerpt();
+                        ?>
+
+                        <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
+                        <p><?php echo $date; ?> by <?php echo $author; ?></p>
+                        <p><?php echo $excerpt; ?></p>
+
+
+                    </div>
+                    <div class="blog-content col-3">
+
+
+                        <?php
+                        $url = get_permalink();
+                        $title = get_the_title();
+                        $date = get_the_date();
+                        $author = get_the_author();
+                        $excerpt = get_the_excerpt();
+                        ?>
+
+                        <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
+                        <p><?php echo $date; ?> by <?php echo $author; ?></p>
+                        <p><?php echo $excerpt; ?></p>
+
+
+                    </div>
                 </div>
             </div>
-        </div>
 
-    <?php endwhile; ?>
-<?php endif; ?>
-
+        <?php endwhile; ?>
+    <?php endif; ?>
 <?php get_footer(); ?>
