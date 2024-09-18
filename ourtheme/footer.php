6d52e88d5589c0fd0663e0476461d1a5cd6d0567
jako3501 <jako3501@easv365.dk>
2024-09-18 13:25:24 +0200

    footer start

--- ourtheme/footer.php
+++ ourtheme/footer.php
@@ -1,5 +1,34 @@
 <?php wp_footer() ?>
-<footer class= 'footer'>
+<footer class= 'footer-container'>
+    <div class="footer-row browse" >
+        <a href="">Produkter</a>
+        <a href="">Læs vores blog</a>
+        <a href="">Doner møbel</a>
+        <a href="">Blog</a>
+    </div>
+    <div class="footer-row info" >
+        <a href="">Om os</a>
+        <a href="">Kundeservice</a>
+        <a href="">Kontakt os</a>
+        
+
+    </div>
+    <div class="footer-row some">
+        <div class="some-logos">
+            <a href="">Ins</a>
+            <a href="">FB</a>
+            <a href="">LI</a>
+            <a href="">TT</a>
+        </div>
+        <div class="nyhedsbrev">
+            <h3>Tilmeld dig vores nyhedsbrev</h3>
+            <input type="text" placeholder="Indtast e-mail her">
+            <button>Tilmeld</button>
+
+        </div>
+
+
+    </div>
 
 
 </footer>
