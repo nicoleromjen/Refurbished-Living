bee8b43e6fb85af34f334a2236e128eeef9fb090
jako3501 <jako3501@easv365.dk>
2024-10-09 12:20:47 +0200

    Update front-page.php

--- ourtheme/front-page.php
+++ ourtheme/front-page.php
@@ -122,7 +122,7 @@
                       ?>
                       <div class="product-card">
                         <div class="product-image">
-                          <img src="<?php the_field('product_image'); ?>" alt="">
+                          <img src="<?php echo esc_url($image['url']['thumbnail']) ?>" alt="">
                         </div>
                         <div class="product-text">
                           <h3><?php echo $title ?></h3>
