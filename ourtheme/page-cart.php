060ec0418871b30729aa7c04415ba133c774c3ba
smw22 <serwes01@easv365.dk>
2024-10-09 13:14:47 +0200

    woocommerce files

---
+++ ourtheme/page-cart.php
@@ -0,0 +1,4 @@
+<?php get_header() ?>
+    <?php echo do_shortcode("[woocommerce_cart]") ?>
+
+<?php get_footer() ?>
---
+++ ourtheme/page-my-account.php
@@ -0,0 +1,4 @@
+<?php get_header() ?>
+    <?php echo do_shortcode("[woocommerce_my_account]") ?>
+
+<?php get_footer() ?>
