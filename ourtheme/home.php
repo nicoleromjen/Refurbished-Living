dc2285894bc00adb820ff0b6a93ab740ddc458ce
jako3501 <jako3501@easv365.dk>
2024-10-20 19:35:35 +0200

    blog style

--- ourtheme/home.php
+++ ourtheme/home.php
@@ -7,32 +7,32 @@
     </div>
 
     <div class="blog-container ">
-        <?php if (have_posts()): ?>
-            <?php while (have_posts()):
-                the_post() ?>
+        
                 <div class="blog-content ">
+                    <?php if (have_posts()): ?>
+                        <?php while (have_posts()):
+                            the_post() ?>
 
-
-                    <?php
-                    $url = get_permalink();
-                    $title = get_the_title();
-                    $date = get_the_date();
-                    $author = get_the_author();
-                    $excerpt = get_the_excerpt();
-                    ?>
+                                <?php
+                                $url = get_permalink();
+                                $title = get_the_title();
+                                $date = get_the_date();
+                                $author = get_the_author();
+                                $excerpt = get_the_excerpt();
+                                ?>
 
                     <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
                     <p><?php echo $date; ?> by <?php echo $author; ?></p>
                     <p><?php echo $excerpt; ?></p>
 
-
+                        <?php endwhile; ?>
+                        <?php endif; ?>
                 </div>
 
             
     </div>
 </div>
 
-<?php endwhile; ?>
-        <?php endif; ?>
+
 
 <?php get_footer(); ?>
