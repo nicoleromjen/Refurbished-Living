6e284dbf440f10834b42a806591fff8f863ecf7b
jako3501 <jako3501@easv365.dk>
2024-10-20 19:59:51 +0200

    Update home.php

--- ourtheme/home.php
+++ ourtheme/home.php
@@ -22,7 +22,7 @@
                                 $thumbnail = get_the_post_thumbnail();
                                 ?>
 
-                    <img src="<?php echo $thumbnail; ?>" alt="">
+                    <?php echo $thumbnail;?>
                     <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
                     <p><?php echo $date; ?> by <?php echo $author; ?></p>
                     <p><?php echo $excerpt; ?></p>
