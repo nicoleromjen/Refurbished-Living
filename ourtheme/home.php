6be894a0a0d0343681c4f4e5c1b6316fbf506ddc
jako3501 <jako3501@easv365.dk>
2024-10-20 19:54:38 +0200

    Update home.php

--- ourtheme/home.php
+++ ourtheme/home.php
@@ -19,9 +19,10 @@
                                 $date = get_the_date();
                                 $author = get_the_author();
                                 $excerpt = get_the_excerpt();
+                                $thumbnail = get_the_post_thumbnail();
                                 ?>
 
-                    <div class="blog-image"><?php the_post_thumbnail('medium') ?></div>
+                    <div class="blog-image"><?php echo $thumbnail; ?></div>
                     <h2><a href="<?php echo $url; ?>"><?php echo $title; ?></a></h2>
                     <p><?php echo $date; ?> by <?php echo $author; ?></p>
                     <p><?php echo $excerpt; ?></p>
